139beffb509f65d4ac11079584cb5798d3f9913c
jakubko443 <jakubsichta211@gmail.com>
2025-10-06 19:59:55 +0200

    Delete #uloha 1.py

--- #uloha 1.py
+++
@@ -1,8 +0,0 @@
-#uloha 1
-a=int(input("Zadaj mi cislo:"))
-b=int(input("Zadaj mi druhe cislo:"))
-if (a*b) < 100:
-    print("cislo je mensie ako 100")
-else :
-    print("cislo je vacsie ako 100")
-    
