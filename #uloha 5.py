6d99e430acb557515061e41c40d98ab67dc7c24e
jakubko443 <jakubsichta211@gmail.com>
2025-12-02 17:37:44 +0100

    Delete #uloha 5.py

--- #uloha 5.py
+++
@@ -1,6 +0,0 @@
-#uloha 5
-N= int(input("zadaj mi hodnotu N(spodna hranica):"))
-X=int(input("zadaj mi hodnotu X(horna hranica):"))
-for i in range(N, X+1):
-    odmocnina=i**0.5
-    print(i,round(odmocnina, 2))
