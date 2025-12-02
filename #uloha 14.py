4edcf91649c7022e8e73ddce164095f112a313e2
jakubko443 <jakubsichta211@gmail.com>
2025-12-02 17:37:01 +0100

    Delete #uloha 14.py

--- #uloha 14.py
+++
@@ -1,8 +0,0 @@
-#uloha 14
-z= int(input("zadaj mi hodnotu z(spodna hranica):"))
-k=int(input("zadaj mi hodnotu k(horna hranica):"))
-pocet_cisel=0
-for i in range (z, k+1):
-    if i%8==0:
-        pocet_cisel+=1
-print (pocet_cisel)
